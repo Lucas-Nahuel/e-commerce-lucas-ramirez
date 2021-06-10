4328b6b16a2d669a7a2a95f5286a03ed96e9ff66
lucas ramirez <lucas01.ramirez@gmail.com>
2021-06-09 23:09:02 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+  render(){ 
+    return (
+    <Fragment>
+      <nav>
+      <ul className="flex">
+        <li>prdocuto1</li>
+        <li>prdocuto2</li>
+        <li>prdocuto3</li>
+      </ul>
+      </nav>
+      <div className="App">
       <h1>my proyect!</h1>
-    </div>
+      </div>
+    </Fragment>
   );
+}
 }
 
 export default App;
