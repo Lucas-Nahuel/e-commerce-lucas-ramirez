71e02c05451af4042e224eb6879da525d90d5023
lucas ramirez <lucas01.ramirez@gmail.com>
2021-06-11 20:44:55 -0300

    navbar v2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
+import './bootstrap.min.css';
 
 
 class App extends Component {
   render(){ 
     return (
     <Fragment>
-      <nav>
-      <ul className="flex">
-        <li>prdocuto1</li>
-        <li>prdocuto2</li>
-        <li>prdocuto3</li>
+      <nav className="navbar navbar-expand-md navbar-light">
+        <a href="/App.js" className="navbar-brand">Brand</a>
+      <div className="collapse navbar-collapse lista"> 
+      <ul className="navbar-nav">
+        <li className="nav-item"><a className="nav-link" id="color-nav" href="/App.js">producto1</a></li>
+        <li className="nav-item"><a className="nav-link" id="color-nav" href="/App.js">producto2</a></li>
+        <li className="nav-item"><a className="nav-link" id="color-nav" href="/App.js">producto3</a></li>
       </ul>
+      </div>
       </nav>
       <div className="App">
       <h1>my proyect!</h1>
